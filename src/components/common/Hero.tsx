3ff0aa92cbddba0339b508750f894387f2f69c1e
peeChulchul <144536397+peeChulchul@users.noreply.github.com>
2024-03-06 15:36:57 +0900

    Update Hero.tsx

--- src/components/common/Hero.tsx
+++ src/components/common/Hero.tsx
@@ -6,8 +6,8 @@ export default function Hero() {
   return (
     <section className="text-center">
       <Image priority className="mx-auto w-[250px] h-[250px] rounded-full" src={profileImage} alt="author image" />
-      <h2 className="text-3xl font-bold mt-2">{"응애"}</h2>
-      <h3 className="text-xl font-semibold">신생아 개발자</h3>
+      <h2 className="text-3xl font-bold mt-2">김현철</h2>
+      <h3 className="text-xl font-semibold">프론트엔드 개발자</h3>
       <p>유통기한 2075-04-23</p>
       <Link href={"/contact"}>
         <button className="bg-yellow-500 font-bold rounded-xl py-1 px-4 mt-2">Contact Me</button>
