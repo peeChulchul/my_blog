2296de536fedee24563f1d1a43202365b615aa92
peeChulchul <eyeless356@naver.com>
2024-03-07 21:36:25 +0900

    footer 저작권표기 수

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
 export default function Footer() {
-  return <footer className="bg-slate-900 text-white py-2 text-sm text-center">PeeChulChul | All Right Reserved</footer>;
+  return (
+    <footer className="bg-slate-900 text-white py-2 text-sm text-center">© 2024 My Blog. All Right Reserved</footer>
+  );
 }
